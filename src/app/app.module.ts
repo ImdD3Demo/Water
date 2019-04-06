ca285972b403dbe12e5b3acc08741a3727542e5d
walter <walter.kuijlaars@gmail.com>
2019-04-06 18:30:49 +0200

    #upandrunningD3.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,18 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './components/header/header.component';
+import { HomeComponent } from './components/home/home.component';
+import { DataComponent } from './components/data/data.component';
+import { BarchartComponent } from './components/barchart/barchart.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    HomeComponent,
+    DataComponent,
+    BarchartComponent
   ],
   imports: [
     BrowserModule,
